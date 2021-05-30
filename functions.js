678c6499bd3e64d3f4320d9a12d27b5c2fafbea6
zerept <thiagoaperez@gmail.com>
2021-05-30 14:55:02 -0300

    Atualização

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
-dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
-monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+dayName = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"]
+monName = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro"]
 now = new Date
 
 function lista(valor){
